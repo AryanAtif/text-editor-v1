b900b43a724a5c334a9a4f34705921510eb5f70f
Aryan Atif <aryanatif01@gmail.com>
2025-11-25 15:35:15 +0500

    Add: the exit_raw_mode() to be run before program's exit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <termios.h>
 
-void enter_raw_mode()
-{
-  struct termios raw;
+struct termios og_termios;
 
-  tcgetattr(STDIN_FILENO, &raw); //  read the terminal attributes into raw - aka - the struct termios
-                                 //
+void exit_og_termios_mode()
+{
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios);
+}
+
+
+void enter_og_termios_mode()
+{
+  tcgetattr(STDIN_FILENO, &og_termios); // read the original terminal attributes.
+                                        
+  atexit(exit_og_termios_mode); // tell the compiler beforehand to execute exit_og_termios_mode() before quiting the program.
+
+  struct termios raw = og_termios; // copy the original terminal attributes into a new termios variable "raw", so that the original attributes don't get disturbed when the program calls exit_og_termios_mode before quitting the program.
+  
   raw.c_lflag &= ~(ECHO);        // AND the ECHO bits with the inverted bits
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);      // set the new terminal attributes to raw - aka - the struct termios
@@ -16,7 +27,7 @@ void enter_raw_mode()
 int main() 
 {
   
-  enter_raw_mode();
+  enter_og_termios_mode();
   char c;
   
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
