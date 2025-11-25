4f1d430cc6a5ac202ae327756dbea15d4cf2343e
Aryan Atif <aryanatif01@gmail.com>
2025-11-25 14:52:39 +0500

    Add: Enable the raw mode

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <unistd.h>
 #include <termios.h>
 
-int main() {
+void enter_raw_mode()
+{
+  struct termios raw;
+
+  tcgetattr(STDIN_FILENO, &raw); //  read the terminal attributes into raw - aka - the struct termios
+                                 //
+  raw.c_lflag &= ~(ECHO);        // AND the ECHO bits with the inverted bits
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);      // set the new terminal attributes to raw - aka - the struct termios
+}
+
+
+int main() 
+{
+  
+  enter_raw_mode();
   char c;
+  
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  
   return 0;
 }
