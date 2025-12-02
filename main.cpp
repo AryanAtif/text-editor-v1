19039c3fb7057a8b8b5c6e64f0265d5bd195c7b7
Aryan Atif <aryanatif01@gmail.com>
2025-12-02 11:11:29 +0500

    Replace: std::endl with /r/n to ensure newline+carridge return

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <ctype.h>
 
 
-struct termios og_termios;
+class termios og_termios;
 
 void exit_raw_mode()
 { tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios);
@@ -18,7 +18,7 @@ void enter_raw_mode()
                                         
   atexit(exit_raw_mode); // tell the compiler beforehand to execute exit_raw_mode() before quiting the program.
 
-  struct termios raw = og_termios; // copy the original terminal attributes into a new termios variable "raw", so that the original attributes don't get disturbed when the program calls exit_og_termios_mode before quitting the program.
+  class termios raw = og_termios; // copy the original terminal attributes into a new termios variable "raw", so that the original attributes don't get disturbed when the program calls exit_og_termios_mode before quitting the program.
   
   raw.c_iflag &= ~(ICRNL | IXON);
   raw.c_oflag &= ~(OPOST);
@@ -40,11 +40,11 @@ int main()
     // The character of ascii values (0-31) are named control characters. they really don't have any symbol assigned to them.
     if ( c >= 0 && c <= 31)  
     {
-      std::cout << int(c) << std::endl;
+      std::cout << int(c) << "\r\n";
     }
     else
     {
-      std::cout << int(c) << " (" << c << ")" << std::endl;
+      std::cout << int(c) << " (" << c << ")" << "\r\n";
     }
   }
    
