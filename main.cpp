a1c224f1f1561b2c8f11de931657036ec628c9df
Aryan Atif <aryanatif01@gmail.com>
2025-12-16 17:43:18 +0500

    Add: the welcome message

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <iostream>
+#include <cstdio>
 #include <cctype>
 #include <cstring>
 #include <cerrno>
@@ -19,6 +20,7 @@
 
 #define CTRL_KEY(k) ((k) & 0x1f) // Define Ctrl+<anyKey> to be 00011111 (which behaves on terminal as ctrl + <anykey>)
 
+#define VERSION  "1.0";
 //==========================================================================================================
 /**** Forward Declarations ***/
 //==========================================================================================================
@@ -149,7 +151,18 @@ void editorProcessKeypress() // editorProcessKeypress() waits for a keypress, an
 void editorDrawRows(AppendBuffer *ab)  // The rows of tildes
 {
   int y;
-  for (y = 0; y < config.screen_rows; y++) {
+  for (y = 0; y < config.screen_rows; y++) 
+  {
+    if(y == config.screen_rows / 3)  // when the "y" is exactly at the 1/3 of the terminal's height
+    {
+      char welcome [50];
+      int welcome_length = snprintf(welcome, sizeof(welcome), "Text editor -- version %s", VERSION);
+
+
+      if(welcome_length > config.screen_cols) { welcome_length = config.screen_cols; } // When the welcome message is too long for some screen.
+    
+      ab->append(welcome);
+    }
     ab->append("~");
 
     ab->append("\x1b[K"); // erarse each line before painting
