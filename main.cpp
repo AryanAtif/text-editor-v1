de2d82cdf01e99178bfa637343a6f5b42b51b25b
Aryan Atif <aryanatif01@gmail.com>
2025-12-17 10:02:38 +0500

    ahh

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <iostream>
+#include <cstdio>
 #include <cctype>
 #include <cstring>
 #include <cerrno>
@@ -18,6 +19,20 @@
 //==========================================================================================================
 
 #define CTRL_KEY(k) ((k) & 0x1f) // Define Ctrl+<anyKey> to be 00011111 (which behaves on terminal as ctrl + <anykey>)
+
+#define VERSION  "1.0"
+
+enum cursor_movement
+{
+  ARROW_LEFT = 1000,
+  ARROW_DOWN,
+  ARROW_UP,
+  ARROW_RIGHT,
+  PAGE_UP,
+  PAGE_DOWN,
+  HOME_KEY,
+  END_KEY
+};
 
 //==========================================================================================================
 /**** Forward Declarations ***/
@@ -32,6 +47,7 @@ void editorRefreshScreen();
 class Editor_config
 {
   public:
+    int cursor_x, cursor_y; // the x and y coordinates of the cursor
     int screen_rows;
     int screen_cols;
 
@@ -74,7 +90,7 @@ void enter_raw_mode()
 }
 
 
-char editorReadKey()  //editorReadKey()’s job is to wait for one keypress, and return it
+int editorReadKey()  //editorReadKey()’s job is to wait for one keypress, and return it
 {
   int nread; // the value returned by read()
   char c;    // The character entered by the user
@@ -84,7 +100,57 @@ char editorReadKey()  //editorReadKey()’s job is to wait for one keypress, and
     if (nread == -1 && errno != EAGAIN) {throw std::runtime_error(std::string("Read error:") + std::strerror(errno));}
 ;
   }
-  return c;
+  if (c == '\x1b') 
+  {
+    char seq[3];
+    
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+    if (seq[0] == '[') 
+    {      
+      if (seq[1] >= '0' && seq[1] <= '9')       // check if the entered sequence represents the pageup/down key
+      {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        
+        if (seq[2] == '~') 
+        {
+          switch (seq[1]) 
+          {
+            case '1': return HOME_KEY;
+            case '4': return END_KEY;
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
+          }
+        }
+      }
+
+      else
+      {
+        switch (seq[1])                      // if not the above, then check if the sequence represents an arrow key
+        {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
+        }
+      }
+      return '\x1b';
+    }
+    else if (seq[0] == 'O') 
+    {
+      switch (seq[1]) 
+      {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
+      }
+    }
+  }  
+    return c;
 }
 
 int getWindowSize(int *rows, int *cols) 
@@ -114,29 +180,92 @@ private:
 
 public:
     void append(std::string_view s) {
-        buffer.append(s);
+        buffer.append(s);                   // calls the std::string::append function to append the string
     }
     
-    // Equivalent to accessing ab->b and ab->len
-    const char* data() const { return buffer.c_str(); }
-    size_t length() const { return buffer.size(); }
+    const char* data() const { return buffer.c_str(); } // returns the C++-style string as a C-style string
+    size_t length() const { return buffer.size(); }     // return the string size
     
-    // abFree is automatic (destructor)
 };
+
+
 //==========================================================================================================
 /**** Input Operations ****/
 //==========================================================================================================
 
+void editorMoveCursor(int key) 
+{
+  switch (key)
+  {
+   case ARROW_LEFT:
+      if (config.cursor_x != 0)
+      {
+        config.cursor_x--;
+      }
+      break;
+
+    case ARROW_RIGHT:
+      if (config.cursor_x != (config.screen_cols -1))
+      {
+        config.cursor_x++;
+      }
+      break;
+
+    case ARROW_UP:
+      if (config.cursor_y != 0)
+      {
+        config.cursor_y--;
+      }
+      break;
+
+    case ARROW_DOWN:
+      if (config.cursor_y != (config.screen_rows -1))
+      {
+        config.cursor_y++;
+      }
+      break;
+  }
+}
+
 void editorProcessKeypress() // editorProcessKeypress() waits for a keypress, and then handles it.
 {
-  char c = editorReadKey();
-  switch (c) {
+  int c = editorReadKey();
+
+  switch (c)
+  {
+
     case CTRL_KEY('q'):
         // To clear the screen
       write(STDOUT_FILENO, "\x1b[2J", 4); // Clears the terminal
       write(STDOUT_FILENO, "\x1b[H", 3);  // Moves the cursor at the top-left of the terminal
       
       exit(0);
+      break;
+
+    // Home/End Key operations
+    case HOME_KEY:
+      config.cursor_x = 0;  // move the cursor at the beginning of the line
+      break;
+
+    case END_KEY:
+      config.cursor_x = config.screen_cols - 1; // move the cursor at the end of the line
+      break;
+
+
+    // Page up/down operations 
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = config.screen_rows;
+        while (times--)                              // While as long as we don't reach the top of the screen
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN); //Keep running the arrow_up key, if the character read was page up
+      }
+      break;
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
+      editorMoveCursor(c);
       break;
   }
 }
@@ -146,27 +275,70 @@ void editorProcessKeypress() // editorProcessKeypress() waits for a keypress, an
 /*** Output Operations ***/
 //==========================================================================================================
 
-void editorDrawRows()  // The rows of tildes
+void editorDrawRows(AppendBuffer *ab)  // The rows of tildes
 {
   int y;
-  for (y = 0; y < config.screen_rows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+
+  for (y = 0; y < config.screen_rows; y++) 
+  {
+    if(y == config.screen_rows / 3)  // when the "y" is exactly at the 1/3 of the terminal's height
+    {
+      char welcome [50];
+      int welcome_length = snprintf(welcome, sizeof(welcome), "Text editor -- version %s", VERSION);
+
+
+      if(welcome_length > config.screen_cols) { welcome_length = config.screen_cols; } // When the welcome message is too long for some screen.
+   
+      int padding = (config.screen_cols - welcome_length) / 2;
+      if (padding)
+      {
+        ab->append("~");
+        padding--;
+      }
+      while (padding--) { ab->append(" "); }
+
+      ab->append(welcome);
+    }
+    else
+    {
+      ab->append("~");
+    }
+
+    ab->append("\x1b[K"); // erarse each line before painting
+    
+    if (y < config.screen_rows - 1) 
+    {
+      ab->append("\r\n");
+    }
   }
 }
 
 void editorRefreshScreen() 
 {
-  write(STDOUT_FILENO, "\x1b[2J", 4); // Clears the terminal
-  write(STDOUT_FILENO, "\x1b[H", 3);  // Moves the cursor at the top-left of the terminal
-  editorDrawRows();
-  write(STDOUT_FILENO, "\x1b[H", 3);  // Moves the cursor at the top-left of the terminal
+  AppendBuffer ab;
+
+  ab.append("\x1b[?25l"); // hides the cursor
+  ab.append("\x1b[H");  // Moves the cursor at the top-left of the terminal
+  editorDrawRows(&ab);
+  
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", config.cursor_y + 1, config.cursor_x + 1);
+  ab.append(buf);
+
+  ab.append("\x1b[?25h"); // shows the cursor
+  write(STDOUT_FILENO, ab.data(), ab.length());
 }
 
 //==========================================================================================================
+// terminal init
 //==========================================================================================================
 
 void initEditor() 
 {
+  // set the x and y coordinates to 0,0
+  config.cursor_x = 0; 
+  config.cursor_y = 0;
+
   if (getWindowSize(&config.screen_rows, &config.screen_cols) == -1) {throw std::runtime_error(std::string("Read error:") + std::strerror(errno));}
 }
 
